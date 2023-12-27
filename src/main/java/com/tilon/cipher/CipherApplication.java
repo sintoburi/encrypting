067ff48926f69683e3438d342369e42bb3439277
play4yuu <fain01@knou.ac.kr>
2023-12-27 14:30:20 +0900

    스프링부트3 추가

---
+++ src/main/java/com/tilon/cipher/CipherApplication.java
@@ -0,0 +1,14 @@
+package com.tilon.cipher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CipherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CipherApplication.class, args);
+        System.out.println("http://localhost:8080");
+    }
+
+}
---
+++ src/test/java/com/tilon/cipher/CipherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tilon.cipher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CipherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
